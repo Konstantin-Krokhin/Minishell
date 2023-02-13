8828bc866c3683ad28320883c820ca77dd1d8c9f
mgranero <mgranero@student.42wolfsburg.de>
2023-02-13 15:04:54 +0100

    basis with one command and one argument

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mgranero <mgranero@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 17:21:04 by mgranero          #+#    #+#             */
-/*   Updated: 2023/01/25 20:40:13 by mgranero         ###   ########.fr       */
+/*   Updated: 2023/02/13 14:22:38 by mgranero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,41 @@ static	void print_intro(void);
  *
  * @return int a number different than 0 in case of error
  */
-int	main(void)
-{
-	char	*buff;
-	char	*cmd;
-	char	**args;
-	int		err_status;
+// int	main(void)
+// {
+// 	char	*buff;
+// 	char	*cmd;
+// 	char	**args;
+// 	int		err_status;
 
-	print_intro();
-	err_status = 0;
-	while (1)
-	{
-		buff = readline(">>");
-		if (ft_strlen(buff) > 0)
-			 add_history(buff);
-		//PARSE INPUT
-		args = 0; //intialized
-		cmd = 0; //intialized
-		// if (parser(buff, cmd, args) == 1)
-		// {
-		// 	err_status = 1;
-		// 	break;
-		// }
-		//CALL COMMANDS
-		//PIPE
-		// REDIRECT
-		free(buff);
-	}
-	if (err_status == 1)
-	{
-		free(buff);
-		write(2, "Error: parser\n", 14);
-		return (1);
-	}
-	return (0);
-}
+// 	print_intro();
+// 	err_status = 0;
+// 	while (1)
+// 	{
+// 		buff = readline(">>");
+// 		if (ft_strlen(buff) > 0)
+// 			 add_history(buff);
+// 		//PARSE INPUT
+// 		args = 0; //intialized
+// 		cmd = 0; //intialized
+// 		// if (parser(buff, cmd, args) == 1)
+// 		// {
+// 		// 	err_status = 1;
+// 		// 	break;
+// 		// }
+// 		//CALL COMMANDS
+// 		//PIPE
+// 		// REDIRECT
+// 		free(buff);
+// 	}
+// 	if (err_status == 1)
+// 	{
+// 		free(buff);
+// 		write(2, "Error: parser\n", 14);
+// 		return (1);
+// 	}
+// 	return (0);
+// }
 
 /**
  * @brief function will print the intro characters by program initialization
